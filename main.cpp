74d9ef481aa7e2108dbfa2418eda5178615497b0
Louis Billaut <ragedfear4875@gmail.com>
2025-09-26 02:31:28 +0800

    implement specular lighting with Phong's algorithm

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 struct Material {
-    Material(const Vec3f &color) : diffuse_color(color) {}
-    Material() : diffuse_color() {}
+    Material(const Vec2f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular_exponent(spec) {}
+    Material() : albedo(1,0), diffuse_color(), specular_exponent() {}
+    Vec2f albedo;
     Vec3f diffuse_color;
+    float specular_exponent;
 };
 
 struct Sphere {
@@ -42,11 +44,13 @@ struct Light {
     float intensity;
 };
 
-const Material ivory(Vec3f(0.4, 0.4, 0.3));
-const Material plastic = Vec3f(0.3, 0.1, 0.1);
+const Material ivory(Vec2f(0.6,  0.3), Vec3f(0.4, 0.4, 0.3), 50.0);
+const Material plastic(Vec2f(0.6,  0.3), Vec3f(0.3, 0.1, 0.1), 10.0);
 
 const Vec3f Background_Color = Vec3f(0.5, 0.5, 0.5);
 const int FOV = 1.05; // 60 Deg FOV
+
+Vec3f reflect(const Vec3f &I, const Vec3f &N) {return I - N*2.f*(I*N);} // Direction vector
 
 bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const vector<Sphere> &spheres, Vec3f &hit, Vec3f &N, Material &material) {
     float dist_i;
@@ -73,12 +77,15 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const vector<Sphere> &sphere
     if (!scene_intersect(orig, dir, spheres, point, N, material)) return Background_Color;
 
     float diffuse_light_intensity = 0;
+    float specular_light_intensity = 0;
     for (size_t i=0; i<lights.size(); i++) {
         Vec3f light_dir = (lights[i].position - point).normalize();
-        diffuse_light_intensity += lights[i].intensity * max(0.f, light_dir*N);
+        diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
+        specular_light_intensity += powf(max(0.f, reflect(light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;
     }
 
-    return material.diffuse_color * diffuse_light_intensity;
+    // See https://en.wikipedia.org/wiki/Phong_reflection_model#Concepts
+    return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1];
 }
 
 void render(const std::vector<Sphere> &spheres, const vector<Light> &lights) {
@@ -105,14 +112,17 @@ void render(const std::vector<Sphere> &spheres, const vector<Light> &lights) {
     ofs << "P6\n" << width << " " << height << "\n255\n";
 
     for (size_t i = 0; i < height*width; ++i) {
+        Vec3f &c = framebuffer[i];
+        float maximum = max(c[0], max(c[1], c[2]));
+        if (maximum > 1) c = c*(1./maximum);
+
         for (size_t j = 0; j < 3; j++) {
-            float clamped_color = max(0.f, min(1.f, framebuffer[i][j]));
+            float clamped_color = max(0.f, min(1.f, c[j]));
             unsigned char final_rgb_color = 255 * clamped_color;
 
             ofs << final_rgb_color;
         }
     }
-
     ofs.close();
 }
 
@@ -125,6 +135,8 @@ int main() {
 
     vector<Light> lights;
     lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
+    lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
+    lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
 
     render(spheres, lights);
     return 0;
