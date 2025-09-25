a1f06bcde8a06873d49aa19af78e16c1c0f30895
Louis Billaut <ragedfear4875@gmail.com>
2025-09-26 01:00:37 +0800

    saving buffer frame as ppm (open with gimp)

--- main.cpp
+++ main.cpp
@@ -15,6 +15,23 @@ void render() {
             framebuffer[i + j*width] = Vec3f(j/float(height), i/float(width), 0);
         }
     }
+
+    // Save buffer frame
+    ofstream ofs;
+    ofs.open("./output/out.ppm");
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+
+    for (size_t i = 0; i < height*width; ++i) {
+        for (size_t j = 0; j < 3; j++) {
+            float clamped_color = max(0.f, min(1.f, framebuffer[i][j]));
+            unsigned char final_rgb_color = 255 * clamped_color;
+
+            ofs << final_rgb_color;
+        }
+    }
+
+    ofs.close();
+
 }
 
 int main() {
