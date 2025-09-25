3cc16b341e43e5774da93bc7653750fe803bbab5
Louis Billaut <ragedfear4875@gmail.com>
2025-09-26 00:37:48 +0800

    initialize buffer frame

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include "geometry.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+void render() {
+    const int width = 1024;
+    const int height = 768;
+    
+    // Initialize buffer frame
+    vector<Vec3f> framebuffer(width * height);
+
+    for (size_t j = 0; j < height; j++) {
+        for (size_t i = 0; i < width; i++) {
+            framebuffer[i + j*width] = Vec3f(j/float(height), i/float(width), 0);
+        }
+    }
+}
+
 int main() {
+    render();
     return 0;
 }
