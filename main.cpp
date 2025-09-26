1756d9ce0165f7b0b81514c2736f9c1ce50281db
Louis Billaut <ragedfear4875@gmail.com>
2025-09-26 13:28:42 +0800

    add scene class, grouping spheres lights and bg

--- main.cpp
+++ main.cpp
@@ -47,6 +47,15 @@ struct Light {
     float intensity;
 };
 
+struct Scene {
+    Scene(const vector<Sphere> &s, const vector<Light> &l) : spheres(s), lights(l) {}
+    vector<Sphere> spheres;
+    vector<Light> lights;
+    unsigned char* bg_data = nullptr;
+
+    ~Scene() {if (bg_data) stbi_image_free(bg_data);}
+};
+
 const Material ivory(1.0, Vec4f(0.6,  0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3), 50.0);
 const Material plastic(1.0, Vec4f(0.9,  0.1, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1), 10.0);
 const Material mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.0);
@@ -55,9 +64,7 @@ const Material glass(1.5, Vec4f(0.0, 0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8), 125.0
 const Vec3f Background_Color = Vec3f(0.5, 0.5, 0.5);
 const float FOV = 1.05; // 60 Deg FOV
 const float PI = 3.14159265359;
-
 int bg_width, bg_height, bg_channels;
-unsigned char* bg_data = stbi_load("assets/dr_sybren.jpg", &bg_width, &bg_height, &bg_channels, 3);
 
 // Direction vector --- See Phong's algorithm
 Vec3f reflect(const Vec3f &I, const Vec3f &N) {
@@ -81,17 +88,17 @@ Vec3f refract(const Vec3f &I, const Vec3f &N, const float &refractive_index) {
     return k < 0 ? Vec3f(0,0,0) : I*eta + n*(eta * cosi - sqrtf(k));
 }
 
-bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const vector<Sphere> &spheres, Vec3f &hit, Vec3f &N, Material &material) {
+bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const Scene &scene, Vec3f &hit, Vec3f &N, Material &material) {
     float dist_i;
     float spheres_dist = numeric_limits<float>::max();
 
-    for (size_t i=0; i < spheres.size(); i++) {
+    for (size_t i=0; i < scene.spheres.size(); i++) {
         // Account for overlapping Sphere
-        if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
+        if (scene.spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
             spheres_dist = dist_i;
             hit = orig + dir*dist_i;
-            N = (hit - spheres[i].center).normalize();
-            material = spheres[i].material;
+            N = (hit - scene.spheres[i].center).normalize();
+            material = scene.spheres[i].material;
         }
     }
 
@@ -99,12 +106,12 @@ bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const vector<Sphere> &
     return spheres_dist<1000;
 }
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, size_t depth=0) {
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Scene &scene, size_t depth=0) {
     Vec3f point, N;
     Material material;
 
     // Compute background texture & pixel coords
-    if (depth > 4 || !scene_intersect(orig, dir, spheres, point, N, material)) {
+    if (depth > 4 || !scene_intersect(orig, dir, scene, point, N, material)) {
         float u = 0.5f + atan2f(dir.z, dir.x) / (2 * PI);
         float v = 0.5f - asinf(dir.y) / PI;
 
@@ -112,9 +119,9 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
         int py = min(bg_height - 1, max(0, int(v * bg_height)));
 
         int index = (py * bg_width + px) * 3;
-        float r = bg_data[index] / 255.0f;
-        float g = bg_data[index + 1] / 255.0f;
-        float b = bg_data[index + 2] / 255.0f;
+        float r = scene.bg_data[index] / 255.0f;
+        float g = scene.bg_data[index + 1] / 255.0f;
+        float b = scene.bg_data[index + 2] / 255.0f;
         return Vec3f(r, g, b);
     };
 
@@ -124,31 +131,31 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
     Vec3f reflect_orig = reflect_dir*N < 0 ? point - N*1e-3 : point + N*1e-3; // Offset for no self-occlusion
     Vec3f refract_orig = refract_dir*N < 0 ? point - N*1e-3 : point + N*1e-3;
 
-    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
-    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
+    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, scene, depth + 1);
+    Vec3f refract_color = cast_ray(refract_orig, refract_dir, scene, depth + 1);
 
     float diffuse_light_intensity = 0;
     float specular_light_intensity = 0;
-    for (size_t i=0; i<lights.size(); i++) {
-        Vec3f light_dir = (lights[i].position - point).normalize();
-        float light_distance = (lights[i].position - point).norm();
+    for (size_t i=0; i<scene.lights.size(); i++) {
+        Vec3f light_dir = (scene.lights[i].position - point).normalize();
+        float light_distance = (scene.lights[i].position - point).norm();
         
         // Check if point in shadow of lights[i]
         Vec3f shadow_orig = light_dir*N < 0 ? point - N*1e-3 : point + N*1e-3;
         Vec3f shadow_pt, shadow_N;
         Material tmpmaterial;
-        if (scene_intersect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt-shadow_orig).norm() < light_distance)
+        if (scene_intersect(shadow_orig, light_dir, scene, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt-shadow_orig).norm() < light_distance)
             continue;
 
-        diffuse_light_intensity  += lights[i].intensity * std::max(0.f, light_dir*N);
-        specular_light_intensity += powf(max(0.f, reflect(light_dir, N)*dir), material.specular_exponent)*lights[i].intensity;
+        diffuse_light_intensity  += scene.lights[i].intensity * max(0.f, light_dir*N);
+        specular_light_intensity += powf(max(0.f, reflect(light_dir, N)*dir), material.specular_exponent)*scene.lights[i].intensity;
     }
 
     // See https://en.wikipedia.org/wiki/Phong_reflection_model#Concepts
     return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity * material.albedo[1] + reflect_color*material.albedo[2] + refract_color*material.albedo[3];
 }
 
-void render(const std::vector<Sphere> &spheres, const vector<Light> &lights) {
+void render(const Scene &scene) {
     const int width = 1024;
     const int height = 768;
     
@@ -162,7 +169,7 @@ void render(const std::vector<Sphere> &spheres, const vector<Light> &lights) {
             float y = -(2*(j + 0.5)/(float)height - 1) * tan(FOV/2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
 
-            framebuffer[i + j*width] = cast_ray(Vec3f(0,0,0), dir, spheres, lights);
+            framebuffer[i + j*width] = cast_ray(Vec3f(0,0,0), dir, scene);
         }
     }
 
@@ -198,7 +205,9 @@ int main() {
     lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
     lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
 
-    render(spheres, lights);
-    stbi_image_free(bg_data);
+    Scene scene(spheres, lights);
+    scene.bg_data = stbi_load("assets/dr_sybren.jpg", &bg_width, &bg_height, &bg_channels, 3);
+
+    render(scene);
     return 0;
 }
